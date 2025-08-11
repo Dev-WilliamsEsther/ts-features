e5b3e649809758d1ad1d9e08d807a533a516549d
Dev-WilliamsEsther <williexther20@gmail.com>
2025-08-11 14:57:40 +0100

    fifth push

--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -3,3 +3,12 @@ const drink = {
     carbonated: true,
     sugar: 40
   };
+
+  // Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+
